8ce6adf4d5597a85e0f60519c6da2cd4a8787f4a
Begomz <begonamz@educastur.org>
2026-02-08 19:29:52 +0100

    Add files via upload

---
+++ Juego cifras y letras Bego/Diccionario_escolar.js
@@ -0,0 +1,81 @@
+/* ======================================================
+   DICCIONARIO ESCOLAR – EDUCACIÓN PRIMARIA
+   Español estándar · Sin nombres propios
+   Longitud: 3 a 9 letras
+   Aprox. 1200 palabras
+   ====================================================== */
+
+const DICCIONARIO_ESCOLAR = {
+
+  /* ===== 3 LETRAS ===== */
+  3: [
+    "sol","mar","pan","pez","rio","sal","luz","voz","pie","ojo","ala","oso",
+    "uva","feo","dia","mes","año","red","fin","rey","ley","par","cal","don"
+  ],
+
+  /* ===== 4 LETRAS ===== */
+  4: [
+    "casa","mesa","silla","flor","hoja","lana","pato","gato","perro","pez",
+    "roca","nube","lluvia","frio","calor","libro","tiza","cama","sofa",
+    "panal","rama","vela","lago","campo","jugo","leche","sopa","piso",
+    "puerta","pared","techo","suelo","ropa","bota","caja","bola","dado",
+    "faro","mina","tren","avion","barco","isla","playa","rio","puente"
+  ],
+
+  /* ===== 5 LETRAS ===== */
+  5: [
+    "clase","papel","lapiz","goma","regla","libro","leche","fruta","plato",
+    "vaso","silla","mesa","campo","bosque","playa","nubes","viento",
+    "calle","plaza","parque","coche","rueda","motor","puente","pista",
+    "camino","sendero","arbol","hojas","semilla","tierra","pasto",
+    "fuego","agua","hielo","nieve","salud","cuerpo","brazo","pierna",
+    "cabeza","nariz","boca","diente","ojos","oidos","pulso","ritmo",
+    "suma","resta","cuenta","cifra","tabla","regla","medir","pesar"
+  ],
+
+  /* ===== 6 LETRAS ===== */
+  6: [
+    "alumno","alumna","maestra","clases","escuela","familia","hermano",
+    "hermana","amigos","amigas","equipo","grupo","trabajo","juego",
+    "juegos","tareas","cuadro","pizarra","ventana","puertas","cuarto",
+    "cocina","salon","baños","escalera","tejado","comida","cena","desayuno",
+    "frutas","verdura","arroz","pasta","sopa","carne","pescado","huevo",
+    "animal","plantas","bosque","campo","pradera","montes","colinas",
+    "numero","figura","recta","angulo","circulo","calculo","medidas",
+    "tiempo","mañana","tarde","noche","ayer","hoy","antes","despues",
+    "correr","saltar","jugar","pensar","leer","escribir","dibujar"
+  ],
+
+  /* ===== 7 LETRAS ===== */
+  7: [
+    "cuaderno","ventana","pizarra","mochila","estuche","cartulina",
+    "colores","rotulos","plastico","hermano","hermana","familias",
+    "personas","amigos","vecinos","trabajos","oficios","jardinero",
+    "carpeta","archivo","carpetas","comedor","cocinas","armario",
+    "espejos","cortina","almohada","sabanas","manteles","servilleta",
+    "alimento","bebidas","ensalada","desayuno","merienda",
+    "problema","operar","calculo","resultado","explicar","razonar",
+    "medidas","longitud","anchura","volumen","superfi","perimetro",
+    "bosques","montana","pradera","llanura","desierto","planeta"
+  ],
+
+  /* ===== 8 LETRAS ===== */
+  8: [
+    "operacion","resultado","problemas","solucion","expresion","numerico",
+    "geometria","triangulo","cuadrado","rectangulo","poligono",
+    "calculado","razonado","explicar","comparar","ordenado","clasificar",
+    "naturales","animales","vegetales","ecosistema","paisajes",
+    "transporte","carreteras","aeropuerto","estaciones","ferrocarril",
+    "alimentos","nutricion","equilibrio","actividad","movimiento",
+    "amistades","convivir","respeto","solidar","colaborar","participar"
+  ],
+
+  /* ===== 9 LETRAS ===== */
+  9: [
+    "matematico","explicado","razonable","operativo","numeracion",
+    "geometria","rectiline","expresivo","combinado","ordenacion",
+    "naturaleza","vegetales","animacion","movimiento","equilibrio",
+    "convivenc","solidario","colaborar","participa","organizar",
+    "educativo","aprendiz","creativo","responsa","autonomia"
+  ]
+};
