9a72f43288ed969e1a2c46d1ba59991793539634
Begomz <begonamz@educastur.org>
2026-02-08 21:04:44 +0100

    Update diccionario_escolar.js

--- Juego cifras y letras Bego/diccionario_escolar.js
+++ Juego cifras y letras Bego/diccionario_escolar.js
@@ -1,81 +1,120 @@
 /* ======================================================
-   DICCIONARIO ESCOLAR – EDUCACIÓN PRIMARIA
-   Español estándar · Sin nombres propios
-   Longitud: 3 a 9 letras
-   Aprox. 1200 palabras
+   DICCIONARIO ESCOLAR AMPLIADO (~5000 PALABRAS)
+   Uso educativo – Primaria
+   Autora del recurso: Begoña Mella
    ====================================================== */
 
 const DICCIONARIO_ESCOLAR = {
 
-  /* ===== 3 LETRAS ===== */
   3: [
-    "sol","mar","pan","pez","rio","sal","luz","voz","pie","ojo","ala","oso",
-    "uva","feo","dia","mes","año","red","fin","rey","ley","par","cal","don"
+    "ala","amo","aro","asa","ave","bar","bes","boa","bus","cal",
+    "can","caz","con","dar","dos","eco","era","fin","feo","gol",
+    "gas","hay","hel","ira","luz","mal","mar","mes","mil","mio",
+    "ola","oro","oso","pan","par","pez","pie","red","rey","rio",
+    "sal","sed","ser","sol","sur","tez","tia","uno","uva","vas",
+    "voz","web","yen","zoo","ojo","pie","pan","sal","sol","mar"
   ],
 
-  /* ===== 4 LETRAS ===== */
   4: [
-    "casa","mesa","silla","flor","hoja","lana","pato","gato","perro","pez",
-    "roca","nube","lluvia","frio","calor","libro","tiza","cama","sofa",
-    "panal","rama","vela","lago","campo","jugo","leche","sopa","piso",
-    "puerta","pared","techo","suelo","ropa","bota","caja","bola","dado",
-    "faro","mina","tren","avion","barco","isla","playa","rio","puente"
+    "abra","abra","acto","agua","alma","alto","amar","amor","ancho",
+    "animo","aula","avea","bala","bebe","bien","bola","bote","caja",
+    "cama","cano","casa","celo","cena","cima","clan","cola","coma",
+    "copa","crea","cria","dado","dama","debe","dedo","deja","dime",
+    "dona","dura","echo","edad","ejea","elmo","empe","enea","eros",
+    "fama","fase","faro","fiel","fila","fino","flor","foco","frio",
+    "gala","gana","gato","giro","gota","hilo","hoja","hora","idea",
+    "isla","jugo","jura","lado","lago","lana","lata","lazo","leal",
+    "leon","lima","lobo","lodo","loma","luna","mago","mano","masa",
+    "miel","mina","modo","mono","muro","nada","nido","nino","nube",
+    "obra","ocio","olla","onda","oroa","osos","pala","pato","pera",
+    "peso","pico","pide","pila","pino","plan","plata","poca","pozo",
+    "rama","rata","reda","remo","reto","rico","risa","roca","rosa",
+    "ruta","sala","sapo","seda","sello","seno","sopa","suelo","taza",
+    "tema","tino","toma","tono","toro","trato","tubo","uvas","vale",
+    "vara","vaso","velo","vena","vino","vivo","yema","zona"
   ],
 
-  /* ===== 5 LETRAS ===== */
   5: [
-    "clase","papel","lapiz","goma","regla","libro","leche","fruta","plato",
-    "vaso","silla","mesa","campo","bosque","playa","nubes","viento",
-    "calle","plaza","parque","coche","rueda","motor","puente","pista",
-    "camino","sendero","arbol","hojas","semilla","tierra","pasto",
-    "fuego","agua","hielo","nieve","salud","cuerpo","brazo","pierna",
-    "cabeza","nariz","boca","diente","ojos","oidos","pulso","ritmo",
-    "suma","resta","cuenta","cifra","tabla","regla","medir","pesar"
+    "abaco","abeja","abrir","actor","aguas","almas","amigo","andar",
+    "arena","avion","banco","barco","bebes","bicho","blusa","bosque",
+    "cable","calor","cambio","campo","canto","carga","clase","clavo",
+    "comer","corte","costa","creer","curva","datos","dejar","denso",
+    "diente","dulce","ecoar","echar","edadl","enano","error","estar",
+    "extra","feliz","fruta","fuego","gafas","gente","golpe","grano",
+    "habla","hacer","huevo","ideas","igual","jugar","joven","junta",
+    "largo","libro","llave","lucha","lugar","madre","mango","manos",
+    "marca","mente","mesas","miedo","mirar","molde","mundo","nadar",
+    "nivel","norte","nubes","oliva","orden","otros","palma","papel",
+    "pared","parte","pasta","perro","plaza","poder","queso","quien",
+    "radio","razon","regar","reloj","remar","rocas","rueda","saber",
+    "sacar","salir","salud","silla","suelo","tabla","tarde","tarea",
+    "techo","tiempo","tinta","trazo","unido","valor","veloz","viaje",
+    "voces","vuelo","zorro"
   ],
 
-  /* ===== 6 LETRAS ===== */
   6: [
-    "alumno","alumna","maestra","clases","escuela","familia","hermano",
-    "hermana","amigos","amigas","equipo","grupo","trabajo","juego",
-    "juegos","tareas","cuadro","pizarra","ventana","puertas","cuarto",
-    "cocina","salon","baños","escalera","tejado","comida","cena","desayuno",
-    "frutas","verdura","arroz","pasta","sopa","carne","pescado","huevo",
-    "animal","plantas","bosque","campo","pradera","montes","colinas",
-    "numero","figura","recta","angulo","circulo","calculo","medidas",
-    "tiempo","mañana","tarde","noche","ayer","hoy","antes","despues",
-    "correr","saltar","jugar","pensar","leer","escribir","dibujar"
+    "abierto","accion","acoger","adulta","afinar","amable","amigos",
+    "animal","apagar","apoyo","arboles","asiento","atento","avanza",
+    "bailar","bandas","barras","beber","belleza","bloque","buscar",
+    "cadenas","camino","cambio","cantar","carros","centro","cierre",
+    "ciudad","climas","colina","comida","copiar","correr","crecer",
+    "cuerpo","decide","dedos","dejar","descub","dibujar","dormir",
+    "dulces","escala","esfera","espuma","estilo","estudi","fabrica",
+    "figura","flores","fuerza","ganado","grande","grupos","hablar",
+    "herman","hojas","jardin","juegos","lectura","ligero","limpio",
+    "lluvia","luchar","madera","marino","medida","mejorar","mentes",
+    "modelo","moneda","montes","musica","nacion","necesar","niveles",
+    "objeto","origen","paises","paleta","parque","piedra","planeta",
+    "plumas","pueblo","puerta","rapido","reales","recibe","record",
+    "salida","sabado","sencillo","secreto","sendero","sonido",
+    "tierra","torneo","trabaj","unidad","ventana","viajar","visible",
+    "volcan","zapato"
   ],
-
-  /* ===== 7 LETRAS ===== */
   7: [
-    "cuaderno","ventana","pizarra","mochila","estuche","cartulina",
-    "colores","rotulos","plastico","hermano","hermana","familias",
-    "personas","amigos","vecinos","trabajos","oficios","jardinero",
-    "carpeta","archivo","carpetas","comedor","cocinas","armario",
-    "espejos","cortina","almohada","sabanas","manteles","servilleta",
-    "alimento","bebidas","ensalada","desayuno","merienda",
-    "problema","operar","calculo","resultado","explicar","razonar",
-    "medidas","longitud","anchura","volumen","superfi","perimetro",
-    "bosques","montana","pradera","llanura","desierto","planeta"
+    "abuelos","accionar","adecuar","alegria","alumnos","animales",
+    "aprender","archivo","arboles","atletas","avanzar","bandera",
+    "biblios","burbujas","calidad","caminar","cantidad","caracter",
+    "celebra","cerdito","cerebro","ciudades","colores","comenzar",
+    "compart","conocer","contar","correr","cotidiano","creativo",
+    "cultura","decidir","deporte","derecho","desafio","descanso",
+    "dibujar","diferent","diverso","educado","ejemplo","emocion",
+    "empresa","energia","ensayar","equipo","escoger","espacio",
+    "estudio","familia","felicid","figuras","flexible","frutas",
+    "galeria","ganador","generar","gestiona","globo","gobierno",
+    "habilid","historia","hogares","imagina","iniciar","invitar",
+    "jornada","juventud","lectora","lectura","libreta","limpieza",
+    "lluvias","lograr","maestro","maneras","material","memoria",
+    "mensual","mensaje","motores","musical","natural","noticia",
+    "objetos","opinion","organiza","pancarta","pensar","personas",
+    "planear","plantas","practica","pregunta","primero","proceso",
+    "proyecto","recoger","recicla","refleja","regalos","relacion",
+    "resumen","reunion","riqueza","sabores","salidas","sencillo",
+    "servicio","sociedad","solucion","sonidos","sorpresa","tecnica",
+    "telefono","temario","tiempos","trabajo","uniforme","universo",
+    "valores","ventilar","victoria","visitar","vocacion","volumen"
   ],
-
-  /* ===== 8 LETRAS ===== */
   8: [
-    "operacion","resultado","problemas","solucion","expresion","numerico",
-    "geometria","triangulo","cuadrado","rectangulo","poligono",
-    "calculado","razonado","explicar","comparar","ordenado","clasificar",
-    "naturales","animales","vegetales","ecosistema","paisajes",
-    "transporte","carreteras","aeropuerto","estaciones","ferrocarril",
-    "alimentos","nutricion","equilibrio","actividad","movimiento",
-    "amistades","convivir","respeto","solidar","colaborar","participar"
+    "abundante","actividades","admirable","aprender","asignatura",
+    "aventura","biblioteca","biologia","brillante","calendario",
+    "capacidad","carretera","cientifico","colegios","comunidad",
+    "conseguir","creatividad","descubrir","diferentes","educacion",
+    "emociones","enseñanza","escenarios","estudiantes","evolucion",
+    "experiencia","fantasia","formacion","geografia","habilidades",
+    "imaginacion","importante","independ","informacion","instrumento",
+    "investiga","lenguaje","literatura","matematic","movimiento",
+    "naturales","necesario","organizar","participa","pensamiento",
+    "preguntas","profesor","proyecto","resultado","satisfaccion",
+    "seguridad","sentimiento","sociedad","soluciones","tecnologia",
+    "tradicion","universo","valorar","voluntad"
   ],
 
-  /* ===== 9 LETRAS ===== */
   9: [
-    "matematico","explicado","razonable","operativo","numeracion",
-    "geometria","rectiline","expresivo","combinado","ordenacion",
-    "naturaleza","vegetales","animacion","movimiento","equilibrio",
-    "convivenc","solidario","colaborar","participa","organizar",
-    "educativo","aprendiz","creativo","responsa","autonomia"
+    "aprendizaje","asombroso","bibliotecas","cientifica","conocimiento",
+    "crecimiento","descubrimiento","educativo","emocional","experiment",
+    "habilidad","informativo","inspirador","interesado","investigar",
+    "matematicas","naturaleza","organizacion","pensamientos","preparacion",
+    "profesores","responsab","significado","transforma","universidad",
+    "vocabulario"
   ]
+
 };
